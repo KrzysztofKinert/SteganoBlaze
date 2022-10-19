0e4b9dcda298b7f5ccf15b2b7e37d7fbd4a505a6
Krzysztof Kinert <krzysztof.kinert@student.po.edu.pl>
2022-10-19 22:00:44 +0200

    audio maxvaluechange v3

--- SteganoBlaze/Shared/MaxValueChange.cs
+++ SteganoBlaze/Shared/MaxValueChange.cs
@@ -2,58 +2,23 @@
 {
     public static class MaxValueChange
     {
-        public static double FloatingPoint(int bits)
+        public static double FloatingPoint(int selectedBits)
         {
-            double maxValue = float.MaxValue + float.MinValue;
-            string binVal = "";
+            if (selectedBits > 29)
+                return 100.0;
 
-            for (int i = 0; i < bits; i++)
+            float maxValue = float.MaxValue;
+
+            string binVal = "01";
+            for (int i = 0; i < 30 - selectedBits; i++)
+                binVal += "0";
+            for (int i = 0; i < selectedBits; i++)
                 binVal += "1";
 
             var byteValue = BitConverter.GetBytes(Convert.ToInt32(binVal, 2));
-            var floatvalue = BitConverter.ToSingle(byteValue, 0);
-           
-            return floatvalue / maxValue * 100;
+            var floatValue = BitConverter.ToSingle(byteValue, 0);
 
-
-            //int bits = 31;
-            //double maxValue = float.MaxValue;
-            //Console.WriteLine(maxValue.ToString());
-
-            //string binVal = "";
-            //int intValue = 1;
-            //string binary = Convert.ToString(intValue, 2);
-            //Console.WriteLine(binary);
-
-            //binVal += "01";
-            //for (int i = 0; i < 29 - bits; i++)
-            //{
-            //    binVal += "0";
-            //}
-            //for (int i = 0; i < bits; i++)
-            //{
-            //    binVal += "1";
-            //}
-
-            //Console.WriteLine(binVal);
-            //intValue = Convert.ToInt32(binVal, 2);
-
-            //var byteValue = BitConverter.GetBytes(intValue);
-            //for (int i = 0; i < byteValue.Length; i++)
-            //{
-            //    Console.WriteLine(Convert.ToString(byteValue[i], 2));
-            //}
-
-            //float floatValue = BitConverter.ToSingle(byteValue, 0);
-
-            //if (float.IsNaN(floatValue) || float.IsInfinity(floatValue))
-            //{
-            //    floatValue = float.MaxValue;
-            //}
-            //Console.WriteLine(floatValue.ToString());
-
-            //var a = Math.Abs(floatValue) / maxValue * 100;
-            //Console.WriteLine(a.ToString());
+            return Math.Log2(floatValue - 1.0) / Math.Log2(maxValue) * 100;
         }
 
         public static double FixedPoint(int selectedBits, int intLength)
